d67137967a9cd107f700c8d558d83247fe521d90
steven kijo <stvnkjm8@gmail.com>
2018-07-17 22:08:38 +0300

    project scaffolding. includes services - weather company data, compose for postgresql, streaming analytics.

---
+++ projectx/settings/development.py
@@ -0,0 +1,7 @@
+from projectx.settings.base import *
+
+DEBUG = True
+
+INSTALLED_APPS += (
+                   # other apps for local development
+                   )
---
+++ projectx/settings/production.py
@@ -0,0 +1,7 @@
+from projectx.settings.base import *
+
+DEBUG = False
+
+INSTALLED_APPS += (
+                   # other apps for production
+                   )
